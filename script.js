0c64d4f252574817f1cbf0cd03e40c1512d769af
sakshi <sakshi@gmail.com>
2024-02-15 23:29:54 +0530

    Updated

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const songs = [
     {
     name: "Song1",
     title:"Baby Calm Down",
-    artist: "XYZ",
+    artist: "Rema",
     },
     {
     name: "Song2",
@@ -47,7 +47,7 @@ const songs = [
     {
     name: "Song3",
     title:"Hiyonat",
-    artist: "abc",
+    artist: "Ummon Guruhi",
     },
     {
     name: "Song4",
@@ -56,8 +56,8 @@ const songs = [
     },
     {
     name: "Song5",
-    title:"Standing By you(Duniya)",
-    artist: "Bangla",
+    title:"Standing By you",
+    artist: "Nish",
     }
 ]
 
@@ -83,6 +83,46 @@ const prevSong = () => {
     loadSong(songs[songIndex]);
     playMusic();
 }
+
+// Progress Bar
+music.addEventListener('timeupdate', (event) => {    //timeupade is an inbuild event
+    // console.log(event)
+    const { currentTime, duration } = event.srcElement;
+    // console.log(currentTime);
+    // console.log(duration);
+
+    let progress_time = (currentTime / duration) * 100;
+    let progress = document.getElementById('progress');
+
+    progress.style.width = `${progress_time}%`
+
+ // music duration update
+    let dur = document.getElementById('duration')
+    let current_time = document.getElementById('current_time')
+    //total time duration is in sec. we have to display it  inn min:sec format
+    let min_Duration = Math.floor(duration / 60);
+    let sec_duration = Math.floor(duration % 60);
+    // console.log(min_Duration)
+    // console.log(sec_duration)
+
+    if(duration)        //if avoid NAN
+    { dur.textContent = `${min_Duration}:${sec_duration}` }
+
+    //current duration update
+    let min_Current = Math.floor(currentTime / 60);
+    let sec_Current = Math.floor(currentTime % 60);
+
+    if (sec_Current < 10) {
+       sec_Current =`0${sec_Current}`;
+   }
+    current_time.textContent = `${min_Current}:${sec_Current}`;
+});
+
+
+//start playing song from where clicked on the progress bar
+
+
+music.addEventListener("ended", nextSong) //1st song ends automatically play next song 
 
 next.addEventListener("click", nextSong);
 prev.addEventListener("click", prevSong);
